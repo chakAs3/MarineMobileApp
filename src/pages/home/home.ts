919b20de684e26cd71930a2844d29939fe63c7d9
Chakir QATAB <javachakir@gmail.com>
2017-02-22 14:32:28 +0400

    Commit from WebStorm home changes

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,21 +8,21 @@ import {AngularFire, AuthProviders, AuthMethods, FirebaseListObservable, Firebas
 import { AppNavService } from '../../app/appNav.service';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController,public af:AngularFire,public appNav:AppNavService) {
-       af.auth.subscribe(data => console.log(data));
-  }
+    constructor(public navCtrl: NavController,public af:AngularFire,public appNav:AppNavService) {
+        af.auth.subscribe(data => console.log(data));
+    }
 
-  gotoContestPage(){
-    this.navCtrl.setRoot(ContestIntroPage).
-    catch(()=> {
-      console.log('should I stay or should I go now');
-       this.navCtrl.setRoot(this.appNav.getPage("login"));
-    })
-  }
+    gotoContestPage(){
+        this.navCtrl.setRoot(ContestIntroPage).
+        catch(()=> {
+            console.log('should I stay or should I go now');
+            this.navCtrl.setRoot(this.appNav.getPage("login"));
+        })
+    }
 
 }
